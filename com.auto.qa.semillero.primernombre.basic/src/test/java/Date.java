114656231ab995bd606a3a1ace2f7b1a13c10954
daniela guzman <danielaguzman50@outlook.es>
2022-10-25 20:22:34 -0500

    función del reto cumpleaños

---
+++ com.auto.qa.semillero.primernombre.basic/src/test/java/Date.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class Date {
+    String date = "25-12-2004";
+    @Test
+    public void birthdayDate(){
+        System.out.printf("Mi fecha de nacimiento es: "+ date);
+    }
+}
