80acb523fddfaa5a06d8244feaa5486bea17133f
marlon-nieves <marlon.3801711612@ucaldas.edu.co>
2022-10-26 11:36:56 -0500

    Feature:26/10/2022, add file reto 2

---
+++ com.auto.qa.semillero.primernombre.basic/src/test/java/TestCrudApis.java
@@ -0,0 +1,47 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+public class TestCrudApis {
+    @Test
+    public void PostLoginUnknown(){
+        //creando una variable y asigando una variable de la libreria restAsure
+        String response = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"pistol\"\n" +
+                        "}")
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().all()
+                .extract().asString();
+
+        System.out.println("La respuesta es:\n" + response);
+    }
+    @Test
+    public void GetListUnknown(){
+        RestAssured.when().get("https://reqres.in/api/unknown").
+                then().log().all();
+    }
+    @Test
+    public void PutUpdateUnknown(){
+        RestAssured
+                .given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"name\": \"Marlon Nieves\",\n" +
+                        "    \"job\": \"QA Automation JR\"\n" +
+                        "}")
+                .put("https://reqres.in/api/users/7")
+                .then()
+                .log().all()
+                .extract().asString();
+    }
+    @Test
+    public void DeleteUnknown(){
+        RestAssured.when().delete("https://reqres.in/api/users/2").then().log().all();
+    }
+}
